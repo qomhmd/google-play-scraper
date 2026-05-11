0568af2d91c4c5ccf7f0868678a30dcabb7a5b05
Mohammad Qodrati <9105400+qomhmd@users.noreply.github.com>
2026-05-11 14:32:42 +0330

    camelCase!

--- test/lib.app.js
+++ test/lib.app.js
@@ -198,8 +198,8 @@ describe('App method', () => {
   it('should fetch PriceText for paid apps properly', () => {
     return gplay.app({ appId: 'com.teslacoilsw.launcher.prime', country: 'in' })
       .then((app) => {
-        const right_hand = app.price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
-        assert.equal(app.priceText, `₹${right_hand}`);
+        const rightHand = app.price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+        assert.equal(app.priceText, `₹${rightHand}`);
         assert.equal(app.currency, 'INR');
       });
   });
